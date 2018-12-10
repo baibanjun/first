fcf2d8696af3a02aa514e4d9878475472ce4a14a
白板君o <291879228@qq.com>
2018-12-10 14:57:13 +0800

    qqq

--- data/data.js
+++ data/data.js
@@ -1,26 +1,32 @@
 //type=1
 
 const all_list = [
-	//
-	{
-		title:'',
-		content:'',
-		type:1,
-		mark:true
-	}
-	,{
-		title:'',
-		content:'',
-		type:1,
-		mark:true
-	}
+  //
+  {
+    title: '1',
+    content: '',
+    type: 1,
+    mark: true
+  }
+	, {
+    title: '2',
+    content: '',
+    type: 1,
+    mark: true
+  }
+	, {
+    title: '3',
+    content: '',
+    type: 1,
+    mark: true
+  }
 
-	//
-	// {
-	// 	id:,
-	// 	title:'',
-	// 	content:'',
-	// 	type:
-	// }
+  //
+  // {
+  // 	id:,
+  // 	title:'',
+  // 	content:'',
+  // 	type:
+  // }
 
 ]
