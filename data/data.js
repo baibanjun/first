ef14f7671cad54bff9236f31148d6e94df96165b
yanli <yanli@flangtech.com>
2019-03-05 11:16:37 +0800

    qqq

--- data/data.js
+++ data/data.js
@@ -1,32 +1,37 @@
-//type=1
+//type= 1js/css相关   2方案库
 
 const all_list = [
   //
+	{
+	  title: 'js小知识点库',
+	  url: 'html/js小知识点库.html',
+	  type: 1,
+	  mark: true
+	},
+	{
+	  title: 'css小知识点库',
+	  url: 'html/css小知识点库.html',
+	  type: 1,
+	  mark: true
+	},
   {
-    title: '1',
-    content: '',
+    title: 'layui常用知识点',
+    url: 'html/layui常用知识点.html',
     type: 1,
     mark: true
-  }
-	, {
-    title: '2',
-    content: '',
-    type: 1,
-    mark: true
-  }
-	, {
-    title: '3',
-    content: '',
-    type: 1,
-    mark: true
-  }
-
-  //
-  // {
-  // 	id:,
-  // 	title:'',
-  // 	content:'',
-  // 	type:
-  // }
+  },
+	{
+	  title: 'bootstrap库样式部分',
+	  url: 'html/bootstrap库样式部分.html',
+	  type: 1,
+	  mark: true
+	},
+	{
+	  title: 'cookie及localStorage使用',
+	  url: 'html/cookie及localStorage使用.html',
+	  type: 1,
+	  mark: true
+	}
+	
 
 ]
