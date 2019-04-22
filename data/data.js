b6c482b88ec4a611dadacd10cb83602f22774cb2
yanli <yanli@flangtech.com>
2019-04-22 18:25:42 +0800

    修改

--- data/data.js
+++ data/data.js
@@ -4,34 +4,41 @@ const all_list = [
   //
 	{
 	  title: 'js小知识点库',
-	  url: 'html/js小知识点库.html',
+	  url: 'html/show_html/js小知识点库.html',
 	  type: 1,
 	  mark: true
 	},
 	{
 	  title: 'css小知识点库',
-	  url: 'html/css小知识点库.html',
+	  url: 'html/show_html/css小知识点库.html',
 	  type: 1,
 	  mark: true
 	},
   {
     title: 'layui常用知识点',
-    url: 'html/layui常用知识点.html',
+    url: 'html/show_html/layui常用知识点.html',
     type: 1,
     mark: true
   },
 	{
 	  title: 'bootstrap库样式部分',
-	  url: 'html/bootstrap库样式部分.html',
+	  url: 'html/show_html/bootstrap库样式部分.html',
 	  type: 1,
 	  mark: true
 	},
 	{
 	  title: 'cookie及localStorage使用',
-	  url: 'html/cookie及localStorage使用.html',
+	  url: 'html/show_html/cookie及localStorage使用.html',
+	  type: 1,
+	  mark: true
+	},
+	{
+	  title: 'vue表单输入绑定',
+	  url: 'html/show_html/vue表单输入绑定.html',
 	  type: 1,
 	  mark: true
 	}
+	
 	
 
 ]
