cf0b24bccd1f071298976237b9dabba8e887a1c5
YanLi <291879228@qq.com>
2020-03-30 18:38:17 +0800

    修改

--- data/data.js
+++ data/data.js
@@ -4,61 +4,61 @@ const all_list = [
   //
 	{
 	  title: 'js小知识点库',
-	  url: 'html/show_html/js小知识点库.html',
+	  url: 'html/showHtml/js小知识点库.html',
 	  type: 1,
 	  mark: true
 	},
 	{
 	  title: 'css小知识点库',
-	  url: 'html/show_html/css小知识点库.html',
+	  url: 'html/showHtml/css小知识点库.html',
 	  type: 1,
 	  mark: true
 	},
   {
     title: 'layui常用知识点',
-    url: 'html/show_html/layui常用知识点.html',
+    url: 'html/showHtml/layui常用知识点.html',
     type: 1,
     mark: true
   },
 	{
 	  title: 'bootstrap库样式部分',
-	  url: 'html/show_html/bootstrap库样式部分.html',
+	  url: 'html/showHtml/bootstrap库样式部分.html',
 	  type: 1,
 	  mark: true
 	},
 	{
 	  title: 'cookie及localStorage使用',
-	  url: 'html/show_html/cookie及localStorage使用.html',
+	  url: 'html/showHtml/cookie及localStorage使用.html',
 	  type: 1,
 	  mark: true
 	},
 	{
 	  title: 'vue表单输入绑定',
-	  url: 'html/show_html/vue表单输入绑定.html',
+	  url: 'html/showHtml/vue表单输入绑定.html',
 	  type: 1,
 	  mark: true
 	},
 	{
 	  title: 'vue-cli项目小知识点',
-	  url: 'html/show_html/vue-cli项目小知识点.html',
+	  url: 'html/showHtml/vue-cli项目小知识点.html',
 	  type: 1,
 	  mark: true
 	},
 	{
 	  title: 'ES6相关特性',
-	  url: 'html/show_html/ES6相关特性.html',
+	  url: 'html/showHtml/ES6相关特性.html',
 	  type: 1,
 	  mark: true
 	},
 	{
 	  title: 'vue项目打包express运行',
-	  url: 'html/show_html/vue项目express服务运行.html',
+	  url: 'html/showHtml/vue项目express服务运行.html',
 	  type: 1,
 	  mark: true
 	},
 	{
 	  title: 'vue-cli3项目完整步骤',
-	  url: 'html/show_html/vue-cli3项目完整步骤.html',
+	  url: 'html/showHtml/vue-cli3项目完整步骤.html',
 	  type: 1,
 	  mark: true
 	}
